b60825cd2548e296b6b701b84bdb9d109fee41ef
vivek3 <adhikari.vivek303@gmail.com>
2019-10-10 02:43:07 +0530

    initial commit

--- vivekweb/settings.py
+++ vivekweb/settings.py
@@ -158,4 +158,10 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
+DEFAULT_FROM_EMAIL = 'AdhikariCloud team <noreply@adhikarivivek.com'
